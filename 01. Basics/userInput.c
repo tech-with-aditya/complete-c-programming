96c1c598266fb63b3960dc117e52c357f294deee
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-30 18:35:06 +0530

    Update userInput.c

--- 01. Basics/userInput.c
+++ 01. Basics/userInput.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
+
 void main() {
-  int num1, num2, sum;
-  printf("Enter the first number: ");
-  scanf("%d", &num1);
-  printf("Enter the second number: ");
-  scanf("%d", &num2);
-  sum = num1 + num2;
-  printf("Sum = %d", sum);
+  int num;
+  printf("Enter a number: ");
+  scanf("%d", &num);
+  printf("You entered %d", num);
 }
