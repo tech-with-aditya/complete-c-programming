0559cfd6ddc9695402520f700653db97a9df4cbd
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-30 21:18:09 +0530

    Update rectangle.c

--- 01. Basics/rectangle.c
+++ 01. Basics/rectangle.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 void main() {
-  double length, breadth, pm, area;
-    
+  double length, breadth, perimeter, area;
+
   printf("Enter the length of the rectangle: ");
   scanf("%lf", &length);
   printf("Enter the breadth of the rectangle: ");
   scanf("%lf", &breadth);
-    
-  pm = 2 * (length + breadth);
+
+  perimeter = 2 * (length + breadth);
   area = length * breadth;
-    
-  // printf("\nThe perimeter of the rectangle is %lf units", pm);
+
+  // printf("\nThe perimeter of the rectangle is %lf units", perimeter);
   // printf("\nThe area of the rectangle is %lf sq. units", area);
-    
+
   // using .2f or .2lf to round off double or floating point number upto 2 decimal places
-  printf("\nThe perimeter of the rectangle is %.2lf units", pm);
+  printf("\nThe perimeter of the rectangle is %.2lf units", perimeter);
   printf("\nThe area of the rectangle is %.2lf sq. units", area);
 }
