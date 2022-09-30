a696b2f69c66384889209c86f931041f8570731e
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-09-30 21:01:54 +0530

    Update arithmeticOperations.c

--- 01. Basics/arithmeticOperations.c
+++ 01. Basics/arithmeticOperations.c
@@ -2,23 +2,23 @@
 
 void main()
 {
-    int a, b, sum, dif, prod, rem;
-    double quot;
+  int a, b, sum, dif, prod, rem;
+  double quot;
     
-    printf("Enter the first number: ");
-    scanf("%d", &a);
-    printf("Enter the second number: ");
-    scanf("%d", &b);
+  printf("Enter the first number: ");
+  scanf("%d", &a);
+  printf("Enter the second number: ");
+  scanf("%d", &b);
     
-    sum = a + b;
-    dif = a - b;
-    prod = a * b;
-    quot = (float)a / b;
-    rem = a % b;
+  sum = a + b;
+  dif = a - b;
+  prod = a * b;
+  quot = (float)a / b;
+  rem = a % b;
     
-    printf("\nSum = %d", sum);
-    printf("\nDifference = %d", dif);
-    printf("\nProduct = %d", prod);
-    printf("\nQuotient = %lf", quot);
-    printf("\nRemainder = %d", rem);
+  printf("\nSum = %d", sum);
+  printf("\nDifference = %d", dif);
+  printf("\nProduct = %d", prod);
+  printf("\nQuotient = %lf", quot);
+  printf("\nRemainder = %d", rem);
 }
