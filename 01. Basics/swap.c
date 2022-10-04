be263d8d8f03dcd4ea25c6cce805601c78035a78
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-10-04 19:04:51 +0530

    Update swap.c

--- 01. Basics/swap.c
+++ 01. Basics/swap.c
@@ -1,20 +1,20 @@
 #include <stdio.h>
 
 void main() {
-    int a, b, temp;
+  int a, b, temp;
     
-    printf("Enter the first number: ");
-    scanf("%d", &a);
-    printf("Enter the second number: ");
-    scanf("%d", &b);
+  printf("Enter the first number: ");
+  scanf("%d", &a);
+  printf("Enter the second number: ");
+  scanf("%d", &b);
     
-    printf("\nNumbers before swap: ");
-    printf("%d %d", a, b);
+  printf("\nNumbers before swap: ");
+  printf("%d %d", a, b);
     
-    temp = a;
-    a = b;
-    b = temp;
+  temp = a;
+  a = b;
+  b = temp;
     
-    printf("\nNumbers after swap: ");
-    printf("%d %d", a, b);
+  printf("\nNumbers after swap: ");
+  printf("%d %d", a, b);
 }
