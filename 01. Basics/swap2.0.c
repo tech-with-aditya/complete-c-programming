80419a24aa433a986b7bdec654c5ae950e2af253
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-10-04 19:03:34 +0530

    Create swap2.0.c

---
+++ 01. Basics/swap2.0.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void main() {
+    int a, b;
+    
+    printf("Enter the first number: ");
+    scanf("%d", &a);
+    printf("Enter the second number: ");
+    scanf("%d", &b);
+    
+    printf("\nNumbers before swap: ");
+    printf("%d %d", a, b);
+    
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    
+    printf("\nNumbers after swap: ");
+    printf("%d %d", a, b);
+}
