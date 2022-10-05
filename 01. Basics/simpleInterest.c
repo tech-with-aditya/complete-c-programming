779304ec262a273dd9a26fda4f6fcc021b85c4a9
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-10-05 22:04:13 +0530

    Update simpleInterest.c

--- 01. Basics/simpleInterest.c
+++ 01. Basics/simpleInterest.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
 
+void main()
+{
+  double principle, time, rate, si;
+
+  printf("Enter the principle (amount): ");
+  scanf("%lf", &principle);
+
+  printf("Enter the time (years): ");
+  scanf("%lf", &time);
+
+  printf("Enter the rate (roi): ");
+  scanf("%lf", &rate);
+
+  si = (principle * rate * time) / 100;
+
+  printf("\nSimple Interest = %.2lf", si);
+}
