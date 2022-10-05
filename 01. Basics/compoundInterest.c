7e8b3ee7ed47877617fd73d5eebceb1ae2eee10b
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-10-05 22:08:36 +0530

    Update compoundInterest.c

--- 01. Basics/compoundInterest.c
+++ 01. Basics/compoundInterest.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <math.h>
 
+void main()
+{
+  double principle, time, rate, ci;
+
+  printf("Enter the principle (amount): ");
+  scanf("%lf", &principle);
+
+  printf("Enter the time (years): ");
+  scanf("%lf", &time);
+
+  printf("Enter the rate (roi): ");
+  scanf("%lf", &rate);
+
+  ci = principle * (pow((1 + rate / 100), time));
+
+  printf("\nCompound Interest = %.2lf", ci);
+}
