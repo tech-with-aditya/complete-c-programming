bf45050990902db66c97c8e2ba4b80c8e36279b4
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-10-31 21:39:38 +0530

    Update create.c

--- 08. Arrays/Operations/create.c
+++ 08. Arrays/Operations/create.c
@@ -1,1 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[100], size, i;
 
+void create() {
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  printf("Enter the elements of the array: ");
+  for(i = 0; i < size; i += 1) {
+    scanf("%d", &a[i]);
+  }
+}
+
+void main() {
+  create();
+}
