66c74c116d76f98a27b131fd6e745fee968e0535
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-10-31 21:40:45 +0530

    Update display.c

--- 08. Arrays/Operations/display.c
+++ 08. Arrays/Operations/display.c
@@ -1,1 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[100], size, i;
 
+void create() {
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  printf("Enter the elements of the array: ");
+  for(i = 0; i < size; i += 1) {
+    scanf("%d", &a[i]);
+  }
+}
+
+void display() {
+  printf("Elements in the array: ");
+  for(i = 0; i < size; i += 1) {
+    printf("%d ", a[i]);
+  }
+}
+
+void main() {
+  create();
+  display();
+}
