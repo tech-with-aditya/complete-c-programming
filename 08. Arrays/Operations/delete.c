87cb9fbbfae77e8671748373a0e19c2ca775cb84
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-10-31 21:40:24 +0530

    Update delete.c

--- 08. Arrays/Operations/delete.c
+++ 08. Arrays/Operations/delete.c
@@ -1,1 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[100], size, i;
 
+void create() {
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  printf("Enter the elements of the array: ");
+  for(i = 0; i < size; i += 1) {
+    scanf("%d", &a[i]);
+  }
+}
+
+void delete() {
+  int pos;
+  printf("\nEnter the position: ");
+  scanf("%d", &pos);
+  for(i = pos - 1; i < size; i += 1) {
+    a[i] = a[i + 1];
+  }
+  size -= 1;
+}
+
+void display() {
+  printf("Elements in the array: ");
+  for(i = 0; i < size; i += 1) {
+    printf("%d ", a[i]);
+  }
+}
+
+void main() {
+  create();
+  display();
+  delete();
+  display();
+}
