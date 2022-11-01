812f3a176ea22cb82c210f23a99a90052f58afac
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-11-01 20:00:54 +0530

    Update count.c

--- 08. Arrays/Operations/count.c
+++ 08. Arrays/Operations/count.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[100], size, i;
 
+void create() {
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  printf("Enter the elements of the array: ");
+  for(i = 0; i < size; i += 1) {
+    scanf("%d", &a[i]);
+  }
+}
+
+void count() {
+  printf("Total number of elements present in the array: %d", size);
+}
+
+void main() {
+  create();
+  count();
+}
