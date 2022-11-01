9c515c9f3b41cc4ef2b22b51204396789d2aea6b
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-11-01 20:00:17 +0530

    Update update.c

--- 08. Arrays/Operations/update.c
+++ 08. Arrays/Operations/update.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[100], size, i;
 
+void create() {
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  printf("Enter the elements of the array: ");
+  for(i = 0; i < size; i += 1) {
+    scanf("%d", &a[i]);
+  }
+}
+
+void update() {
+  int pos;
+  printf("\nEnter the position to update: ");
+  scanf("%d", &pos);
+  printf("Enter updated element: ");
+  scanf("%d", &a[pos - 1]);
+}
+
+void display() {
+  printf("Elements in the array: ");
+  for(i = 0; i < size; i += 1) {
+    printf("%d ", a[i]);
+  }
+}
+
+void main() {
+  create();
+  display();
+  update();
+  display();
+}
