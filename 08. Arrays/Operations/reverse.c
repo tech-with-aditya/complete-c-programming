df5ff2b482efb7f4bbb1e839d2c12d9a1ceca2c5
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-11-02 21:44:31 +0530

    Update reverse.c

--- 08. Arrays/Operations/reverse.c
+++ 08. Arrays/Operations/reverse.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[100], size, i;
 
+void create() {
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  printf("Enter the elements of the array: ");
+  for(i = 0; i < size; i += 1) {
+    scanf("%d", &a[i]);
+  }
+}
+
+void reverse() {
+  int temp;
+  for(i = 0; i < size/2; i += 1) {
+    temp = a[i];
+    a[i] = a[size - i - 1];
+    a[size - i - 1] = temp;
+  }
+}
+
+void display() {
+  printf("Elements in the array: ");
+  for(i = 0; i < size; i += 1) {
+    printf("%d ", a[i]);
+  }
+}
+
+void main() {
+  create();
+  reverse();
+  display();
+}
