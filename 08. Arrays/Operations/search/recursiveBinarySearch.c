b4b6ac5189bb6e65a2fa342bde03d5ee3875e35c
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-11-03 20:53:08 +0530

    Update recursiveBinarySearch.c

--- 08. Arrays/Operations/search/recursiveBinarySearch.c
+++ 08. Arrays/Operations/search/recursiveBinarySearch.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[100], size, i;
 
+void create() {
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  printf("Enter the elements of the array: ");
+  for(i = 0; i < size; i += 1) {
+    scanf("%d", &a[i]);
+  }
+}
+
+int recBinarySearch(int a[], int start, int end, int x) {
+  int mid;
+  if(start <= end) {
+        mid = start + (end - start) / 2;
+        if(a[mid] == x) {
+            return mid + 1;
+        }
+        if(a[mid] > x) {
+            return recBinarySearch(a, start, mid - 1, x);
+        }
+        else {
+            return recBinarySearch(a, mid + 1, end, x);
+        }
+        return -1;
+  }
+}
+
+void result() {
+  int x;
+  printf("\nEnter the element to search: ");
+  scanf("%d", &x);
+  int target = recBinarySearch(a, 0, size - 1, x);
+  printf("Element found at position: %d", target);
+}
+
+void display() {
+  printf("Elements in the array: ");
+  for(i = 0; i < size; i += 1) {
+    printf("%d ", a[i]);
+  }
+}
+
+void main() {
+  create();
+  display();
+  result();
+}
