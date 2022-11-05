3e4997fb0b75008f8ae44bf01c4936b88f75cf51
Aditya Rawat <112870829+tech-with-aditya@users.noreply.github.com>
2022-11-05 23:22:03 +0530

    Update bubbleSort.c

--- 08. Arrays/Operations/sort/bubbleSort.c
+++ 08. Arrays/Operations/sort/bubbleSort.c
@@ -1,1 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+int a[100], size, i;
 
+void create() {
+  printf("Enter the size of the array: ");
+  scanf("%d", &size);
+  printf("Enter the elements of the array: ");
+  for(i = 0; i < size; i += 1) {
+    scanf("%d", &a[i]);
+  }
+}
+
+void bubbleSort() {
+  int temp, j;
+  for(i = 0; i < size - 1; i += 1) {
+    for(j = 0; j < size - i - 1; j += 1) {
+      if(a[j] > a[j + 1]) {
+        temp = a[j];
+        a[j] = a[j + 1];
+        a[j + 1] = temp;
+      }
+    }
+  }
+}
+
+void display() {
+  printf("Elements in the array: ");
+  for(i = 0; i < size; i += 1) {
+    printf("%d ", a[i]);
+  }
+}
+
+void main() {
+  create();
+  bubbleSort();
+  display();
+}
